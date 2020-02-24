788ce6b3f9ed9ad0f826916c2ebc5a3bc2edff9a
andrey <andreyzasulevichdev@gmail.com>
2020-02-24 22:41:35 +0300

    init: start reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    var str = String(n)
+        .split("")
+        .reverse()
+        .join("");
+    var newStr = str;
+    if (str.charAt(str.length - 1) == "0") newStr = str.slice(0, -1);
+    if (str.charAt(str[0]) == "0") newStr = str.slice(1);
+    return newStr;
+};
